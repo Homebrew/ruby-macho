8d80bb7607d89b8cbd653e1f7b002fcef93f80ae
William Woodruff <william@tuffbizz.com>
2015-10-21 00:22:08 -0400

    add more tests

--- test/test_macho.rb
+++ test/test_macho.rb
@@ -1,14 +1,22 @@
 require 'minitest/autorun'
+require 'digest/sha1'
 require 'macho'
 
 class MachOFileTest < Minitest::Test
+	def equal_sha1_hashes(file1, file2)
+		digest1 = Digest::SHA1.file(file1).to_s
+		digest2 = Digest::SHA1.file(file2).to_s
+
+		digest1 == digest2
+	end
+
 	def test_executable
-		file = MachO::MachOFile.new("test/bin/as.bin")
+		file = MachO::MachOFile.new("test/bin/hello.bin")
 
 		# a file can only be ONE of these
 		assert file.executable?
-		assert !file.dylib?
-		assert !file.bundle?
+		refute file.dylib?
+		refute file.bundle?
 
 		assert_equal MachO::MH_CIGAM_64, file.magic
 		assert_equal "MH_CIGAM_64", file.magic_string
@@ -30,9 +38,9 @@ class MachOFileTest < Minitest::Test
 		file = MachO::MachOFile.new("test/bin/thin.dylib")
 
 		# a file can only be ONE of these
-		assert !file.executable?
+		refute file.executable?
 		assert file.dylib?
-		assert !file.bundle?
+		refute file.bundle?
 
 		assert_equal MachO::MH_CIGAM_64, file.magic
 		assert_equal "MH_CIGAM_64", file.magic_string
@@ -45,6 +53,37 @@ class MachOFileTest < Minitest::Test
 
 		# it's a dylib, so it *must* have a dylib id
 		assert file.dylib_id
+
+		assert file.segments.size > 0
+		assert file.linked_dylibs.size > 0
+	end
+
+	def test_bundle
+		file = MachO::MachOFile.new("test/bin/attr.so")
+
+		# a file can only be ONE of these
+		refute file.executable?
+		refute file.dylib?
+		assert file.bundle?
+
+		assert_equal MachO::MH_CIGAM_64, file.magic
+		assert_equal "MH_CIGAM_64", file.magic_string
+		assert_equal "MH_BUNDLE", file.filetype
+		assert file.cputype
+		assert file.cpusubtype
+		assert file.ncmds
+		assert file.sizeofcmds
+		assert file.flags
+
+		# it's not a dylib, so it has no dylib id
+		assert_nil file.dylib_id
+
+		assert file.segments.size > 0
+		assert file.linked_dylibs.size > 0
+	end
+
+	def test_change_dylib_id
+		file = MachO::MachOFile.new("test/bin/thin.dylib")
 
 		# changing the dylib id should work
 		old_id = file.dylib_id
@@ -64,29 +103,30 @@ class MachOFileTest < Minitest::Test
 		assert_raises MachO::HeaderPadError do
 			file.dylib_id = really_big_id
 		end
+
+		# TODO: compare actual and expected file hashes
 	end
 
-	def test_bundle
-		file = MachO::MachOFile.new("test/bin/attr.so")
+	def test_change_install_name
+		file = MachO::MachOFile.new("test/bin/hello.bin")
 
-		# a file can only be ONE of these
-		assert !file.executable?
-		assert !file.dylib?
-		assert file.bundle?
+		dylibs = file.linked_dylibs
 
-		assert_equal MachO::MH_CIGAM_64, file.magic
-		assert_equal "MH_CIGAM_64", file.magic_string
-		assert_equal "MH_BUNDLE", file.filetype
-		assert file.cputype
-		assert file.cpusubtype
-		assert file.ncmds
-		assert file.sizeofcmds
-		assert file.flags
+		# there should be at least one dylib linked to the binary
+		refute_empty dylibs
 
-		# it's not a dylib, so it has no dylib id
-		assert_nil file.dylib_id
+		file.change_install_name(dylibs[0], "test")
+		new_dylibs = file.linked_dylibs
 
-		assert file.segments.size > 0
-		assert file.linked_dylibs.size > 0
+		# the new dylib name should reflect the changes we've made
+		assert_equal "test", new_dylibs[0]
+		refute_equal dylibs[0], new_dylibs[0]
+
+		file.write("test/bin/hello_actual.bin")
+
+		# compare actual and expected file hashes, to ensure file correctness
+		assert equal_sha1_hashes("test/bin/hello_actual.bin", "test/bin/hello_expected.bin")
+	ensure
+		File.delete("test/bin/hello_actual.bin")
 	end
 end
