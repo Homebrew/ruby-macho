1f697680524c206db3c8bf543731f4d73f5263ab
Martin Afanasjew <martin@afanasjew.de>
2016-02-17 16:50:27 +0100

    bin/info.rb: Improve output readability
    Indent information in different sections with two spaces. Also add size
    information to sections and file offset/size information to segments.

--- bin/info.rb
+++ bin/info.rb
@@ -7,34 +7,33 @@ require 'macho'
 file = MachO::MachOFile.new(ARGV.shift)
 
 puts "FILE INFORMATION:"
-puts "Header type: #{file.header.class}"
-puts "Magic: #{file.magic_string}"
-puts "Filetype: #{file.filetype}"
-puts "CPU type: #{file.cputype}"
-puts "CPU subtype: #{file.cpusubtype}"
-puts "No. load commands: #{file.ncmds}"
-puts "Size of load commands: #{file.sizeofcmds}"
-puts "Flags: #{file.flags}"
-
+puts "  Header type: #{file.header.class}"
+puts "  Magic: #{file.magic_string}"
+puts "  Filetype: #{file.filetype}"
+puts "  CPU type: #{file.cputype}"
+puts "  CPU subtype: #{file.cpusubtype}"
+puts "  No. load commands: #{file.ncmds}"
+puts "  Size of load commands: #{file.sizeofcmds}"
+puts "  Flags: #{file.flags}"
 
 puts "\nLOAD COMMANDS:"
 file.load_commands.each do |lc|
-  puts "#{lc} (#{lc.class}) (offset: #{lc.offset}, size: #{lc.cmdsize})"
+  puts "  #{lc} (#{lc.class}) (offset: #{lc.offset}, size: #{lc.cmdsize})"
 end
 
 puts "\nDYLIB ID: #{file.dylib_id}" if file.dylib?
 
 puts "\nDYNAMIC LIBRARIES:"
-
-puts file.linked_dylibs.join("\n")
+puts "  #{file.linked_dylibs.join("\n  ")}"
 
 puts "\nSEGMENTS AND SECTIONS:"
 
 file.segments.each do |seg|
-  puts "SEGMENT: #{seg.segname}"
+  puts "  Segment: #{seg.segname} " \
+       "(offset: #{seg.fileoff}, size: #{seg.filesize})"
 
   file.sections(seg).each do |sect|
-    puts "\tSECTION: #{sect.section_name}"
-    puts "\t\tOFFSET: #{sect.offset}"
+    puts "    Section: #{sect.section_name} " \
+         "(offset: #{sect.offset}, size: #{sect.size})"
   end
 end
