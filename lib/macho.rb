7e85119673a4b46889ac49d578cab04742a5dba2
William Woodruff <william@yossarian.net>
2018-07-03 12:01:38 -0400

    macho: 2.0.0

--- lib/macho.rb
+++ lib/macho.rb
@@ -12,7 +12,7 @@ require_relative "macho/tools"
 # The primary namespace for ruby-macho.
 module MachO
   # release version
-  VERSION = "1.3.0.pre.1".freeze
+  VERSION = "2.0.0".freeze
 
   # Opens the given filename as a MachOFile or FatFile, depending on its magic.
   # @param filename [String] the file being opened
