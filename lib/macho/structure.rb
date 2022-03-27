b1e37390f5294145651064aa97b8fb91235d3e85
apainintheneck <apainintheneck@gmail.com>
2022-03-27 00:35:02 -0700

    Refactored MachOStructure class

--- lib/macho/structure.rb
+++ lib/macho/structure.rb
@@ -4,52 +4,79 @@ module MachO
   # A general purpose pseudo-structure.
   # @abstract
   class MachOStructure
+    # array of fields in definition order
     @field_list = []
-    @sizeof = 0
+
+    # total size of binary chunk
+    @bytesize = 0
+
+    # binary format string
     @format = ""
+
+    # map of bitmasks
     @mask_map = {}
 
+    class << self
+      # Public getters
+      attr_reader :bytesize, :format, :field_list, :mask_map
+
+      private
+
+      # Private setters
+      attr_writer :bytesize, :format, :field_list, :mask_map
+    end
+
+    # Used to dynamically create an instance of the inherited class
+    # according to the defined fields.
+    # @param args [Array[x]] list of field parameters
     def initialize(*args)
       raise ArgumentError, "Invalid number of arguments" if args.size != self.class.field_list.size
 
       # Set up all instance variables
       self.class.field_list.zip(args).each do |field, value|
         value = LCStr.new(self, value) if field == :lcstr
-        value &= ~self.class.mask_map(field) if mask_map.key?(field)
+        value &= ~self.class.mask_map[field] if self.class.mask_map.key?(field)
 
         instance_variable_set("@#{field}", value)
       end
     end
 
+    # @param subclass [Class] subclass type
+    # @api private
+    def self.inherited(subclass)
+      # Clone all class instance variables
+      field_list = @field_list.clone
+      bytesize = @bytesize.clone
+      fmt = @format.clone
+      mask_map = @mask_map.clone
+
+      # Add those values to the inheriting class
+      subclass.class_eval do
+        @field_list = field_list
+        @bytesize = bytesize
+        @format = fmt
+        @mask_map = mask_map
+      end
+    end
+
     # @param name [Symbol] name of internal field
     # @param type [Symbol] type of field in terms of binary size
-    # @param size [Int] an optional size parameter for string types
+    # @param fmt [String] optional binary format for custom types
+    # @param size [Int] optional size parameter for custom types
+    # @param mask [Int] optional bitmask
     # @api private
     def self.field(name, type, fmt:, size:, mask:)
       raise ArgumentError, "Invalid field type #{type}" unless Fields::FORMAT_CODE.key?(type)
-      raise ArgumentError, "Missing custom type arguments :fmt and/or :size" unless type != :custom && fmt && size
-      raise ArgumentError, "Invalid field size #{size}" unless !size || size >= 0
+      raise ArgumentError, "Missing custom type arguments :fmt and/or :size" if type == :custom && fmt && size
 
       # Add new field attribute that will be initialized later
       attr_reader name
 
       # Add new field to list and calculate size and format
-      @field_list << name.to_sym
-      @sizeof += Fields::BYTE_SIZE[type] || size
+      @field_list << name
+      @bytesize += Fields::BYTE_SIZE[type] || size
       @format += Fields::FORMAT_CODE[type] || fmt
       @mask_map[name] = mask if mask
-    end
-
-    def self.inherited(subclass)
-      subclass.field_list = @field_list.clone
-      subclass.sizeof = @sizeof.clone
-      subclass.format = @format.clone
-      subclass.mask_map = @mask_map.clone
-    end
-
-    class << self
-      attr_reader :field_list, :sizeof, :format, :mask_map
-      alias bytesize sizeof
     end
 
     # @param endianness [Symbol] either `:big` or `:little`
@@ -70,13 +97,6 @@ module MachO
           "bytesize" => self.class.bytesize,
         },
       }
-    end
-
-    private
-
-    # Needed for self.inherited method
-    class << self
-      attr_writer :field_list, :sizeof, :format, :mask_map
     end
   end
 end
