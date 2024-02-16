3c84e1066c81803e0a2158f68b190b71c7dafc3e
William Woodruff <william@yossarian.net>
2024-02-16 10:02:41 -0500

    macho: 4.0.1
    Signed-off-by: William Woodruff <william@yossarian.net>

--- lib/macho.rb
+++ lib/macho.rb
@@ -16,7 +16,7 @@ require_relative "macho/tools"
 # The primary namespace for ruby-macho.
 module MachO
   # release version
-  VERSION = "4.0.0"
+  VERSION = "4.0.1"
 
   # Opens the given filename as a MachOFile or FatFile, depending on its magic.
   # @param filename [String] the file being opened
